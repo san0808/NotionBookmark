dcb88b223d555a0174cfd512e457457689ee4fbd
Sanket <sanketbhat882002@gmail.com>
2023-06-29 08:42:31 +0530

    working first template => title and url are working for now

---
+++ server.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const { Client } = require("@notionhq/client");
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+
+app.use(bodyParser.json());
+const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
+
+app.post("/api/bookmarks", async (req, res) => {
+    try {
+      const { title, languages, url, tags, notes } = req.body;
+  
+      // Create an object with the bookmark data
+      const bookmark = {
+        title,
+        languages,
+        url,
+        tags,
+        notes,
+      };
+  
+      // Save the bookmark to the Notion database
+      const databaseId = process.env.NOTION_DATABASE_ID; 
+  
+      const response = await notion.pages.create({
+        "parent": { 
+          "type": "database_id",
+          "database_id": databaseId 
+        },
+        "properties": {
+          "Name": {
+            "title": [
+              {
+                "text": {
+                  "content": bookmark.title,
+                },
+              },
+            ],
+          },
+          "URL": {
+            "url": url,
+          },
+          // "Tags": {
+          //  " multi_select": [
+          //     {
+          //       "name": bookmark.tags,
+          //     },
+          //   ],
+          // },
+          // "Notes": {
+          //  " rich_text": [
+          //     {
+          //       "text": {
+          //         "content": bookmark.notes,
+          //       },
+          //     },
+          //   ],
+          // },
+        },
+      });
+      
+  
+      res.status(200).json({ message: "Bookmark saved successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "An error occurred while saving the bookmark" });
+    }
+  });
+  
+
+const port = 5000; // Replace with your desired port number
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
